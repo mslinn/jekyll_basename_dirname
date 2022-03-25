fbed0011f3e54a575e18314a25e40bfb1f82b071
Mike Slinn <mslinn@micronauticsresearch.com>
2022-03-25 12:14:30 -0400

    -

--- lib/jekyll_basename_dirname.rb
+++ lib/jekyll_basename_dirname.rb
@@ -37,5 +37,5 @@ module JekyllBasenameDirnameName
   end
 end
 
-PluginMetaLogger.info { "Loaded #{JekyllBasenameDirnameName::PLUGIN_NAME} v#{JekyllBasenameDirname::VERSION} plugin." }
+PluginMetaLogger.instance.info { "Loaded #{JekyllBasenameDirnameName::PLUGIN_NAME} v#{JekyllBasenameDirname::VERSION} plugin." }
 Liquid::Template.register_filter(JekyllBasenameDirnameName)
