11880bbe0a9c522b72c9f069c560d0efe37fe25b
Mike Slinn <mslinn@mslinn.com>
2023-02-21 15:07:48 -0500

    -

--- lib/jekyll_basename_dirname.rb
+++ lib/jekyll_basename_dirname.rb
@@ -1,40 +1,38 @@
-# frozen_string_literal: true
-
-require "jekyll"
-require "jekyll_plugin_logger"
-require_relative "jekyll_basename_dirname/version"
+require 'jekyll'
+require 'jekyll_plugin_logger'
+require_relative 'jekyll_basename_dirname/version'
 
 # @author Copyright 2020 Michael Slinn
 # @license SPDX-License-Identifier: Apache-2.0
 #
 # Jekyll filters for working with paths.
 module JekyllBasenameDirnameName
-  PLUGIN_NAME = "jekyll_basename_dirname"
+  PLUGIN_NAME = 'jekyll_basename_dirname'.freeze
 
   _logger = PluginMetaLogger.instance.new_logger(self, PluginMetaLogger.instance.config)
 
   # Filters a string containing a path.
   # @return [String] the filename extracted from the path, including the filetype.
-  # @example Extracts "filename.ext" from the path
-  #   {{ "blah/blah/filename.ext" | basename }}
+  # @example Extracts 'filename.ext' from the path
+  #   {{ 'blah/blah/filename.ext' | basename }}
   def basename(filepath)
     File.basename(filepath)
   end
 
   # Filters a string containing a path.
   # @return [String] the portion of th path before the filename and extension.
-  # @example Extracts "blah/blah" from the path.
-  #   {{ "blah/blah/filename.ext" | dirname }}
+  # @example Extracts 'blah/blah' from the path.
+  #   {{ 'blah/blah/filename.ext' | dirname }}
   def dirname(filepath)
     File.dirname(filepath)
   end
 
   # Filters a string containing a path.
   # @return the filename without the extension.
-  # @example Extracts "filename" from the path.
-  #   {{ "blah/blah/filename.ext" | basename_without_extension }}
+  # @example Extracts 'filename' from the path.
+  #   {{ 'blah/blah/filename.ext' | basename_without_extension }}
   def basename_without_extension(filepath)
-    File.basename(filepath).split(".")[0...-1].join(".")
+    File.basename(filepath).split('.')[0...-1].join('.')
   end
 end
 
--- spec/basename_dirname_spec.rb
+++ spec/basename_dirname_spec.rb
@@ -1,19 +1,17 @@
-# frozen_string_literal: true
-
-require "jekyll"
-require "fileutils"
-require_relative "../lib/jekyll_basename_dirname"
+require 'jekyll'
+require 'fileutils'
+require_relative '../lib/jekyll_basename_dirname'
 
 RSpec.describe(JekyllBasenameDirnameName) do
-  include JekyllBasenameDirnameName
+  include described_class
 
-  it "verifies basename" do
-    expect(basename("a/b/c/d/e.html")).to eq("e.html")
-    expect(basename("blah.html")).to eq("blah.html")
+  it 'verifies basename' do
+    expect(basename('a/b/c/d/e.html')).to eq('e.html')
+    expect(basename('blah.html')).to eq('blah.html')
   end
 
-  it "verifies dirname" do
-    expect(dirname("a/b/c/d/e.html")).to eq("a/b/c/d")
-    expect(dirname("blah.html")).to eq(".")
+  it 'verifies dirname' do
+    expect(dirname('a/b/c/d/e.html')).to eq('a/b/c/d')
+    expect(dirname('blah.html')).to eq('.')
   end
 end
